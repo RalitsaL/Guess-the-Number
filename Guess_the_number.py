4a04417c0c463f7eb2ad33decb8417f7bb3bc176
Ralitsa Todorova <ralitsa.yakimova@gmail.com>
2024-09-23 13:45:41 +0300

    Update on the game
    added function

--- Guess_the_number.py
+++ Guess_the_number.py
@@ -1,22 +1,40 @@
 import random
 
 pc_num = random.randint(1, 100)
-print("Guess the number :) hint for you: it is in the range from 1 to 100: ")
+print("Do you want to play a game? Please answer with yes or no.")
+user_answer = input()
 
-while True:
+def start_game():
 
-    player_input = input()
 
-    if not player_input.isdigit():
-        print("Wrong input. Please type a number.")
-        continue
+    print("Guess the number :) hint for you: it is in the range from 1 to 100: ")
+    while True:
 
-    player_input = int(player_input)
-    if player_input == pc_num:
-        print("Well done! You guess it!")
-        break
-    elif player_input > pc_num:
-        print("Try again, this one is a little bit too high.")
-    elif player_input < pc_num:
-        print("Try again. This one is a little bit too low.")
+        player_input = input()
+
+        if not player_input.isdigit():
+            print("Wrong input. Please type a number.")
+            continue
+
+        player_input = int(player_input)
+        if player_input == pc_num:
+            print("Well done! You guess it!")
+            print("Do you want to play again?")
+            user_answer_2 = input("Please answer with yes or no.")
+            if user_answer_2 == "yes":
+                start_game()
+            else:
+                print("Have a great day!")
+                break
+        elif player_input > pc_num:
+            print("Try again, this one is too high.")
+        elif player_input < pc_num:
+            print("Try again. This one is too low.")
+
+
+if user_answer == "yes":
+
+    start_game()
+else:
+    print("Have a great day!")
 
